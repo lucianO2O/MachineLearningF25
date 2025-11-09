55dfc5ce18f5d47f1087c1097fc9e4fa06753309
Luciano Elish <lucianoelish05@gmail.com>
2025-11-09 01:10:29 -0500

    Added confusion matrix display for logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,11 +1,13 @@
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 from sklearn.compose import ColumnTransformer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, ConfusionMatrixDisplay, confusion_matrix
 
 filteredDf = pd.read_csv('CSV_files/filtered_Df.csv')
 randomizedDf = filteredDf.sample(frac = 1, random_state = 42)
@@ -21,7 +23,7 @@ ct = ColumnTransformer(transformers=[("ohe", OneHotEncoder(handle_unknown='ignor
 )
 # choose n_components for SVD
 svd = TruncatedSVD(n_components = 150, random_state=42) # 150 features max
-# pipeline from scikit, makes fitting data EXTREMELY easy
+# pipeline from scikit, makes fitting data EXTREMELY easy, especially when working with encoding for categorical columns. using over "model"
 pipeline = Pipeline([("ct", ct), ("svd", svd), ("clf", LogisticRegression(max_iter=1000))]) # essentially just allows me to put preprocessors (to cut down on the # of columns (one hot encoder creates 10s of thousands for my data)) in order to make the data more workable, max_iter = 1000 for logistic regression to balance convergence and perforance
 X = randomizedDf[categorical + numeric]
 y = randomizedDf['recommendation']
@@ -38,3 +40,14 @@ X_ct = pipeline.named_steps['ct'].transform(X_train) # named_steps allows access
 X_svd = pipeline.named_steps['svd'].transform(X_ct)
 print(X_ct.shape, "after ColumnTransformer")    # printing shape after pipeline transformations
 print(X_svd.shape, "after SVD")
+# visualization
+# general info about cleaned dataset
+print(randomizedDf.info())
+print(randomizedDf.describe())
+print(randomizedDf['recommendation'].value_counts()) # counts how many of each output the column has (both 7000, I did this in the data cleaning step)
+
+# confusion matrix tells which classes the model is confusing
+disp = ConfusionMatrixDisplay(confusion_matrix = confusion_matrix(y_test, y_pred), display_labels = pipeline.classes_)
+disp.plot(cmap='Blues')
+plt.title("Confusion Matrix | Logistic Regression")
+plt.show()
