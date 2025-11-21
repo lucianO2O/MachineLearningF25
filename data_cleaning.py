5709c763b5416ac4e7c012fc0f2aef7fc0aaa753
Luciano Elish <lucianoelish05@gmail.com>
2025-11-21 12:24:32 -0500

    Changed method to clean tags column to clean every categorical column, added comparison to get minimum equal # of rows for both output classes

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,36 +6,39 @@ import ast
 gamesData = pd.read_csv('CSV_files/games_march2025_full.csv')
 # show all columns when printing
 pd.set_option('display.max_columns', None)
-
 # get rid of any duplicate rows
 gamesData = gamesData.drop_duplicates()
 # get rid of playtest games that have no data
 gamesData = gamesData[~gamesData['name'].str.contains('playtest', case = False, na = False)]
-# want to remove # of people who voted for tags in the column and just leave the tag
-gamesData['tags'] = gamesData['tags'].str.replace(r'[0-9:]', '', regex=True) # gets rid of the numbers and colon
-# want to change empty cells to null, they are currently strings with nothing inside in the tags column, but checking all columns for continuity's sake
+# want to change empty cells to null, they are currently strings with nothing inside
 for column in gamesData.columns:    # loops through all columns
     cleanedColumn = []      # temp set for cleaned values
     for cell in gamesData[column]:      # loops through each specific cell in column
-        if pd.isna(cell) or isinstance(cell, str) and cell.strip() in ("[]", "{}", "") or isinstance(cell, list) and len(cell) == 0:  # if the value is already NaN OR take the cell, remove any spaces around it, and then check to see if string matches "[]", "{}", ""
+        if (pd.isna(cell) or
+                isinstance(cell, str) and cell.strip() in ("[]", "{}", "") or
+                isinstance(cell, list) and len(cell) == 0):  # if the value is already NaN OR take the cell, remove any spaces around it, and then check to see if string matches "[]", "{}", ""
             cleanedColumn.append(np.nan)    # convert the empty cell to an ACTUAL null
         else: cleanedColumn.append(cell)    # else just append it
     gamesData[column] = cleanedColumn   # set column equal to the new cleaned one
-# want to convert tags column from string looking lists to actual lists
-cleanedTags = []    # empty set to store the new cleaned column
-for cell in gamesData['tags']:
-    if cell in ('None', 'NaN', 'nan' ''):
-        cleanedTags.append(np.nan)      # if NaN already, then just append to this new empty set
-    elif isinstance(cell, str) and cell.startswith('{') and cell.endswith('}'):     # make sure it's a string
-        try:
-            cleanedTags.append(list(ast.literal_eval(cell)))   # literal eval turns the cell from a string that
-                                                               # looks like a literal, into a python object, and the list()
-                                                               # function turns it into, a more usable, list, which then
-                                                               # gets appended to the new column
-        except (ValueError, SyntaxError):   # good practice to have exception with ast.literal_eval for safety issues
-            cleanedTags.append(np.nan)
-    else: cleanedTags.append(cell)
-gamesData['tags'] = cleanedTags     # assign cleaned values to the column
+
+# want to convert categorical columns from string looking lists to actual lists
+categoricalColumns = ["categories", "genres", "tags", "developers", "publishers"]    # empty set to store the columns i want to clean
+cleanedColumn = []
+for column in gamesData.categoricalColumns:
+    for cell in gamesData[column]:
+        if cell in ('None', 'NaN', 'nan', ''):
+            cleanedColumn.append(np.nan)      # if NaN already, then just append to this new empty set
+        elif isinstance(cell, str) and cell.startswith('{') and cell.endswith('}') or cell.startswith('[') and cell.endswith(']'):     # make sure it's a string
+            try:
+                cleanedColumn.append(list(ast.literal_eval(cell)))   # literal eval turns the cell from a literal into a python object, and the list()
+                                                                   # function turns it into, a more usable, list, which then
+                                                                   # gets appended to the new column
+            except (ValueError, SyntaxError):   # good practice to have exception with ast.literal_eval for safety issues
+                cleanedColumn.append(np.nan)
+        else: cleanedColumn.append(cell)
+    gamesData[column] = cleanedColumn
+# want to remove # of people who voted for tags in the column and just leave the tag
+gamesData['tags'] = gamesData['tags'].str.replace(r'[0-9:]', '', regex=True) # gets rid of the numbers and colon
 # get rid of any unnecessary columns
 gamesData = gamesData.drop(columns = {'appid', 'release_date', 'dlc_count', 'required_age',
                                       'detailed_description', 'about_the_game',
@@ -58,10 +61,14 @@ gamesData = gamesData.rename(columns = {'pct_pos_total': 'pos_rev_pct'})
 gamesData['recommendation'] = (gamesData['pos_rev_pct'] >= 80).astype(int) # percentage of positive reviews is greater or equal to 80 (the threshold for games with steam ratings 'Very Positive' or above. must have at least 50 positive reviews)
 # make a filter to chop down number of entries
 revFilter = gamesData[gamesData['num_reviews_total'] >= 100]
-# randomly selects entries with over 100 reviews equaling to 14k, splitting them equally in half based on target variable
+# randomly selects entries with over 100 reviews, splitting them equally in half based on target variable
+num_pos = revFilter[revFilter['recommendation'] == 1].shape[0] # number of rows w/ output = 1
+num_neg = revFilter[revFilter['recommendation'] == 0].shape[0] # number of rows w/ output = 0
+n = min(num_pos, num_neg) # takes minimum of both
 filteredDf = pd.concat([
-    revFilter[revFilter['recommendation'] == 1].sample(7000, random_state=42), # essentially creating two dataframes and then combining them (.concat)
-    revFilter[revFilter['recommendation'] == 0].sample(7000, random_state=42) ])
+    revFilter[revFilter['recommendation'] == 1].sample(n, random_state=42), # ensures both classes have same number of rows
+    revFilter[revFilter['recommendation'] == 0].sample(n, random_state=42)
+])
 filteredDf.to_csv('CSV_files/filtered_Df.csv', index = False)
 
 print(filteredDf.shape)
